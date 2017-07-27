2898533870bb3d8b9c0f2ae8bf23ea61446d7d3c
variy hang <variy@qq.com>
2017-07-27 19:06:06 +0800

    222

---
+++ src/pages/index/a.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+	    <h1 v-bind:class="{'a': tab==0}">hahaha</h1>
+	    <div class="txt-c mrb-12">
+	        <div class="btn-group">
+	            <button type="button"class="btn btn-default" v-bind:class="{'btn-primary':tab== 0}" @click="tab=0">登陆</button>
+	            <button type="button" class="btn btn-default" v-bind:class="{'btn-primary':tab== 1}" @click="tab=1">注册</button>
+	        </div>
+	    </div>
+	    <div class="input-group mrb-12">
+	        <span class="input-group-addon">用户名</span>
+	        <input type="text" class="form-control" placeholder="Username">
+	    </div>
+	    <div class="input-group mrb-12">
+	        <span class="input-group-addon">&nbsp;密码</span>
+	        <input type="password" class="form-control" placeholder="password">
+	    </div>
+	    <button type="button" class="btn btn-default pull-right">确定</button>
+	</div>
+</template>
+<script>
+	module.exports = {
+		data: function(){
+			return {
+				tab: 0
+			}
+		}
+	}
+</script>
--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -1,14 +1,27 @@
+var index = require('./a.vue');
+$(function(){
+	Vue.component('index', index);
+    new Vue({
+        el: '#container',
+        template: '<index></index>',
+        components: {
+            index: index
+        }
+    })
+})
+// alert(tpl({name: 'world'}))
+// debugger;
+// Zepto('<div></div>').html(tpl({name: 'world'})).appendTo('body')
+// $.ajax({
+// 	url: '/user/login',
+// 	type: 'post',
+// 	data: {
+// 		username: 'adog',
+// 		password: '1234'
+// 	},
+// 	success: function(data) {
+// 		console.log(data)
+// 	}
+// });
 
-$.ajax({
-	url: '/user/login',
-	type: 'post',
-	data: {
-		username: 'adog',
-		password: '1234'
-	},
-	success: function(data) {
-		console.log(data)
-	}
-});
-
-Vue
+// Vue
