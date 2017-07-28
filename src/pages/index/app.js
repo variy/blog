4f90e2a35f5a6ed028af5d0be9221c2a7e3bd6bd
variy hang <variy@qq.com>
2017-07-28 17:31:08 +0800

    vue 修正

--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -9,11 +9,22 @@ $(function(){
    //        settings: App
    //    }
 	}).$mount('#app');
-
+	console.log(ArticleList)
 	new Vue({
+	  	render: h => h(ArticleList),
+
 		template: '<articles></articles>',
 		components: {
 		  articles: ArticleList
 		}
 	}).$mount('#article-box');
+
+	// ArticleList.data = [
+	// 	{
+	// 						title: '111',
+	// 						tagList: ['111'],
+	// 						introduce: '222',
+	// 						reviewCount: 6
+	// 					}
+	// ]
 });
--- src/pages/index/article-list.vue
+++ src/pages/index/article-list.vue
@@ -1,16 +1,27 @@
 <tempalte>
-	<articleitem v-for="item in articleList" :title="item.title" :tagList="item.tagList" :introduce="item.introduce" :reviewCount="item.reviewCount">
+	<articleitem v-for="item in articleList" :title="item.title" :tagList="item.tagList" :introduce="item.introduce" :reviewCount="item.reviewCount"> {{ item.title}}
 	</articleitem>
 </tempalte>
 <script>
-	import articleitem from './article.vue';
+	import article from './article.vue';
 	export default {
-		components: {
-			articleitem: articleitem
-		},
+		// components: {
+		// 	articleitem: article
+		// },
+		render: h => h(article),
 		data: function(){
 			return {
 				articleList: [{
+					title: '111',
+					tagList: ['111'],
+					introduce: '222',
+					reviewCount: 6
+				}, {
+					title: '111',
+					tagList: ['111'],
+					introduce: '222',
+					reviewCount: 6
+				}, {
 					title: '111',
 					tagList: ['111'],
 					introduce: '222',
